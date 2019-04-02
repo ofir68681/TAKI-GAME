4b8a47b14468080ceec54c5e6c5381e2e6e956c8
ofir68681 <ofir68681@gmail.com>
2019-04-02 16:00:53 +0300

    constructor for player

--- Player.cpp
+++ Player.cpp
@@ -1,8 +1,24 @@
 #include "Player.h"
 
+
+
+Player::Player(string name, int num_of_cards){
+	for(int i=0; i<this.num_of_cards; i++){
+		Card c=generate_card();
+		this.cards.push_back(c); 
+	}
+
+
+}
+
+
+
 bool Player::play(Card& current){
 	return true;
 }
 int Player::numberOfCards(){ 
 	return 0;
 }
+
+
+
--- Player.h
+++ Player.h
@@ -1,14 +1,22 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "Card.h"
+#include <string>
+#include <vector>
+
 class Player {
 private:
-	// name;
-	// num_of_cards;
-	// container of cards
+	 string name;
+	 int num_of_cards;
+	std::vector <Cards*> cards; // container of cards
 	// more private members
 
 public: 
+	
+	Player(string name, int num_of_cards) : name{clr}, num_of_cards{num_of_cards}; //default construtor and init list constructor
+// 		this-> players.push_back(p);
+
+
 	bool play(Card& current);
 	//the function get the current card on the table, return true if played, false if took a card from the deck
 	
@@ -20,3 +28,6 @@ public:
 
 
 
+//
+//	Card(const Card& card) : clr{card.clr}, s{card.s} {}; //copy constructor
+//	Card& operator=(const Card& card) {clr = card.clr; s = card.s; return *this;}; //operator=
